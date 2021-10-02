261dfd857bc475cf31974d88b73932e9fdeb089b
subhashra <subhash.rskp@gmail.com>
2021-10-02 15:41:22 +0530

    add backend statefile

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     backend "s3" {
     bucket = "testingdevops"
     encrypt = true
-    key = "state.tfstate"
+    key = "state/terraform.tfstate"
     region  = "us-east-1"
     #profile = "~/.aws/credentials" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
   }
