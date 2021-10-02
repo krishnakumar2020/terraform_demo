1f0d80a381834d3cd231dfcc859490121ee822fe
subhashra <subhash.rskp@gmail.com>
2021-10-02 16:40:35 +0530

    share credentials from local

--- main.tf
+++ main.tf
@@ -1,18 +1,32 @@
 provider "aws" {
   region  = "us-east-1"
   version = "~> 3.38.0"
-  #shared_credentials_file = "~/.aws/credentials"
-  
-
 }
+
 
 terraform {
-    backend "s3" {
-      bucket = "testingdevops"
-      encrypt = true
-      key = "state/terraform.tfstate"
-      region  = "us-east-1"
-      shared_credentials_file = "~/.aws/credentials"
-      #profile = "~/.aws/credentials" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
+  backend "s3" {
+    bucket = "testingdevops"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
+#provider "aws" {
+#  region  = "us-east-1"
+#  version = "~> 3.38.0"
+#  #shared_credentials_file = "~/.aws/credentials"
+#  
+#
+#}
+#
+#terraform {
+#    backend "s3" {
+#      bucket = "testingdevops"
+#      encrypt = true
+#      key = "state/terraform.tfstate"
+#      region  = "us-east-1"
+#      shared_credentials_file = "~/.aws/credentials"
+#      #profile = "~/.aws/credentials" # you have to give the profile name here. not the variable("${var.AWS_PROFILE}")
+#  }
+#}
